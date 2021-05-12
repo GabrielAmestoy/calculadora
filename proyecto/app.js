ce1fa75c087bff5aaa3a369bbab972e7c8cad24e
GabrielAmestoy <gabrielamestoy1993@gmail.com>
2021-05-12 02:39:38 -0300

    Cree el archivo de productos

---
+++ proyecto/app.js
@@ -0,0 +1,30 @@
+/* Gaby@Gaby-PC MINGW32 ~/Desktop/Comision6/Proyecto (master)
+$ mkdir proyecto
+
+Gaby@Gaby-PC MINGW32 ~/Desktop/Comision6/Proyecto (master)
+$ cd proyecto
+
+Gaby@Gaby-PC MINGW32 ~/Desktop/Comision6/Proyecto/proyecto (master)
+$ git config user.name "GabrielAmestoy"
+
+Gaby@Gaby-PC MINGW32 ~/Desktop/Comision6/Proyecto/proyecto (master)
+$ git config user.email gabrielamestoy1993@gmail.com
+
+Gaby@Gaby-PC MINGW32 ~/Desktop/Comision6/Proyecto/proyecto (master)
+$ git status
+On branch master
+
+No commits yet
+
+Untracked files:
+  (use "git add <file>..." to include in what will be committed)
+        
+
+nothing added to commit but untracked files present (use "git add" to track) 
+
+Gaby@Gaby-PC MINGW32 ~/Desktop/Comision6/Proyecto/proyecto (master)
+$ git add app.js
+
+Gaby@Gaby-PC MINGW32 ~/Desktop/Comision6/Proyecto/proyecto (master)
+$ git status */
+
